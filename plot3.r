2db811dc8ad01d16e298271ddfef096afeb2cbc6
adriana2015 <adrianavalcu@gmail.com>
2015-03-08 19:17:52 -0500

    all

--- plot3.r
+++ plot3.r
@@ -1,15 +1,18 @@
+#read data
 data<-read.table("R:/AMVALCU/Analysis/household_power_consumption.txt", header=TRUE,sep =";", na.strings="?")
 names(data)
+#transform data in data format
 data$data1<-paste(data$Date, data$Time)
 data$Date2<-strptime(data$data1,"%d/%m/%Y %H:%M:%S")
+#filter the desired data
 datag<-data[((data$Date2>="2007-02-01 00:00:00")&(data$Date2<="2007-02-03 00:00:00")),]
-
-
+#plot the data
+png()
+png( file = "r:/amvalcu/assignment3/data/plot3.png",width = 480, height = 480, units = "px", pointsize = 15,bg= "white",type = c("windows", "cairo", "cairo-png") )
 p1<-with(datag, plot(Date2, Sub_metering_1, type="l",xlab="",ylab= "Energy Submetering"))
 lines(datag$Date2, datag$Sub_metering_2, type="l", col="red")
 lines(datag$Date2, datag$Sub_metering_3, type="l", col="blue")
 legend("topright", lty=1, col=c("black","red","blue"), legend= c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.5)
 
-dev.copy(png, file = "r:/amvalcu/assignment3/data/plot3.png",width = 480, height = 480, units = "px" ) 
 
 dev.off()
--- plot4.r
+++ plot4.r
@@ -1,21 +1,23 @@
+#read data
 data<-read.table("R:/AMVALCU/Analysis/household_power_consumption.txt", header=TRUE,sep =";", na.strings="?")
 names(data)
+#work on dates
 data$data1<-paste(data$Date, data$Time)
 data$Date2<-strptime(data$data1,"%d/%m/%Y %H:%M:%S")
 datag<-data[((data$Date2>="2007-02-01 00:00:00")&(data$Date2<="2007-02-03 00:00:00")),]
-
-
+#plot
+png()
+png (file = "plot4.png",width = 480, height = 480, units = "px", pointsize = 17,bg= "white",type = c("windows", "cairo", "cairo-png") )
 par(mfrow=c(2,2))
-with(datag, { 
+with(datag, {
   plot(Date2,Global_active_power, type="l", xlab="",ylab="Global Active Power")
   plot(Date2, Voltage, type="l", xlab="daytime", ylab="Voltage")
-  p1<-with(datag, plot(Date2, Sub_metering_1, type="l",ylab= "Energy Submetering"))
+  p1<-with(datag, plot(Date2, Sub_metering_1, type="l",ylab= "Energy Submetering",, xlab="" ))
   lines(datag$Date2, datag$Sub_metering_2, type="l", col="red")
   lines(datag$Date2, datag$Sub_metering_3, type="l", col="blue")
-  legend("topright", lty=1, col=c("black","red","blue"), legend= c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  legend("topright", lty=1, col=c("black","red","blue"), legend= c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.5, xjust=0, yjust=0, bty="n")
   plot(Date2, Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="daytime")
 })
 
-dev.copy(png, file = "plot4.png",width = 480, height = 480, units = "px" ) 
 
 dev.off()
